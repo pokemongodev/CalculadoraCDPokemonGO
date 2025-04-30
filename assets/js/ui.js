4dddf0a0d950f0abda43e6e0c959ebc6b94678f6
leagueofperez <leagueofperez@users.noreply.github.com>
2025-04-30 15:03:36 +0000

    deploy: 54fee9679fd2952c71b5609976b9633fe59b688b

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,20 +1,19 @@
 export function showResults(origen, destino, distance, unit, hours, minutes) {
   const unitNames = {
-    km: 'Kilómetros',
+    km: 'km.',
     mi: 'Millas',
     m: 'Metros'
   };
   const unitName = unitNames[unit] || 'Kilómetros';
-
-  $('#resultado').html(`
-    La distancia entre: <span class="info">${origen}</span> y 
-    <span class="info">${destino}</span> es de 
-    <span class="info">${distance} ${unitName}</span><br>
-    El tiempo estimado es: <span class="info">${('0' + hours).slice(-2)} horas y ${('0' + minutes).slice(-2)} minutos</span>
-  `);
+  $('#resultado').html(
+    `<p class="text-justify">La distancia en línea recta entre: 
+    <span class="info">${origen}</span> y <span class="info">${destino}</span> es de: 
+    <span class="info">${distance}${unitName}</span><br>
+    El tiempo estimado es de: 
+    <span class="info">${('0' + hours).slice(-2)} horas y ${('0' + minutes).slice(-2)} minutos</span></p>`
+  );
   $('#outputModal').modal('show');
 }
-
 export function clearInputs() {
   $('#origen, #destino')
     .val('')
@@ -23,13 +22,11 @@ export function clearInputs() {
     .attr('aria-label', function () {
       return $(this).attr('id') === 'origen' ? 'Origen' : 'Destino';
     });
-
   $('#unit')
     .removeClass('is-invalid')
     .find('option[value=""]')
     .text('Selecciona una unidad de medida');
   $('#unit').prop('selectedIndex', -1).val('');
-
   $("label[for='origen']").text('Origen');
   $("label[for='destino']").text('Destino');
   $("label[for='unit']").text('Unidad');
