cc5cda9087bc5efed1338eadb643c471ac842335
leagueofperez <leagueofperez@gmail.com>
2025-05-06 16:10:32 +0200

    06-05-2025, 16:10:32

--- lintstagedrc.mjs
+++ lintstagedrc.mjs
@@ -1,7 +1,8 @@
-export default {
+const lintStaged = {
   // Specify multiple folders to lint
   'src/**/*': (filenames) => `prettier --write ${filenames.join(' ')}`,
   'src/**/*.js': (filenames) => `eslint --fix ${filenames.join(' ')}`,
   '**/*.mjs': (filenames) => `eslint --fix ${filenames.join(' ')}`,
   'src/**/*.css': (filenames) => `stylelint --fix ${filenames.join(' ')}`
 };
+export { lintStaged };
